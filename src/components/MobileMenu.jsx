ad8bcb8a03010983ad0dcdecd87d5ea9ed09e89d
Amadeo Bon <amadeosph@outlook.com>
2025-08-03 15:40:54 -0300

    `Update MobileMenu component: change text for "/rastreamento-de-pets" link`

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -186,7 +186,7 @@ export default function MobileMenu({ open, onClose }) {
                     "/novo-hospital-veterinario-curitiba",
                     "Novo Hosp. Vet. Curitiba",
                   ],
-                  ["/rastreamento-de-pets", "Como localizar animais desaparecidos"],
+                  ["/rastreamento-de-pets", "Ferramenta para Pets Perdidos"],
                 ]}
               />
               <Accordion
